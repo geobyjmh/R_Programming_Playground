827bfd9fc912d059dddbf9b1f78ac76cc51135b5
julian <julian5595@outlook.com>
2023-12-11 21:36:57 +0000

    first play with dplyr

--- Data_wrangling01/wrangling_play.r
+++ Data_wrangling01/wrangling_play.r
@@ -2,3 +2,15 @@
 library(dplyr)
 library(tidyverse)
 
+
+data <- read.csv("playdata.csv")
+head(data)
+
+
+selected_data <- data %>% 
+  select(names, Age) %>%
+  mutate(age_next_year = Age + 1) %>%
+  filter(Age > 30)
+
+head(selected_data)
+
