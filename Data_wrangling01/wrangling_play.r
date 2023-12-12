5171e2c8fa0449a44f3723cc52e1beb4ce6ed818
julian <julian5595@outlook.com>
2023-12-12 21:32:56 +0000

    more with dplyr

--- Data_wrangling01/wrangling_play.r
+++ Data_wrangling01/wrangling_play.r
@@ -8,9 +8,13 @@ head(data)
 
 
 selected_data <- data %>% 
-  select(names, Age) %>%
+  select(names, Age, Country) %>%
   mutate(age_next_year = Age + 1) %>%
-  filter(Age > 30)
+  filter(Age > 30) %>% 
+  distinct() %>% 
+  arrange(Age) %>% 
+  group_by(Country) %>% 
+  count()
 
 head(selected_data)
 
