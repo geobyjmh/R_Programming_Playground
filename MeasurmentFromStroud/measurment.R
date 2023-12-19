f00ff167579927b56811ba21d3392f401e94045f
julian <julian5595@outlook.com>
2023-12-19 13:29:49 +0000

    added the SD() command for a comparison

--- MeasurmentFromStroud/measurment.R
+++ MeasurmentFromStroud/measurment.R
@@ -18,12 +18,22 @@ data01 <- data %>%
 
 head(data01)
 
-value_variance <- mean(data01$deviation2)
+# the SD command uses n-1 for sampled data which is why value_sd1 and value_sd2 
+# are not the same.
+#value_variance <- sum(data01$deviation2)/(length(data01$deviation2) - 1) # Sampled data
+value_variance <- mean(data01$deviation2) # not sampled data
 print(value_variance)
 
-value_sd <- sqrt(value_variance)
-print(value_sd)
+value_sd1 <- sqrt(value_variance)
+print(value_sd1)
+
+
+value_sd2 <- sd(data$Value, na.rm = FALSE)
+print(value_sd2)
+
 
 summary(data$Value)
+
+
 
 
